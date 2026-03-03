f6b949882fec7abee643602850de94132dd202a3
MO5E2 <76234330+MO5E2@users.noreply.github.com>
2026-03-02 19:09:56 -0500

    Setup structure I modified the React boilerplate to fit the structure of the website and simplified it down, as well as importing Tailwind CSS and DaisyUI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import PageOne from "./components/PageOne";
+import PageTwo from "./components/PageTwo";
+import "./App.css";
+import Earth from "./assets/Earth.png";
+import Sun from "./assets/Sun.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main
+      className="min-h-screen min-h bg-cover bg-center"
+      style={{ backgroundImage: `url(${Earth})` }}
+    >
+      <Navbar />
+      <PageOne />
+      <PageTwo />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/PageTwo.jsx
@@ -0,0 +1,3 @@
+export default function PageTwo() {
+  return <p>Two</p>;
+}
